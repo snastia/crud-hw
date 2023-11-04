a49372430de59662ed426166152b5293b21aa29d
Настя Скрицька <snastia.m@gmail.com>
2023-11-04 11:48:59 +0200

    adsfa

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const formEl = document.querySelector(".js-form")
 const btnListEl = document.querySelector(".btn-list")
 const listEl = document.querySelector(".js-list")
 
+// get
+
 function getMovies(){
     return fetch(`${BASE_URL}`)
     .then(response => response.json())
@@ -23,6 +25,9 @@ const newMovie = {
     director: "Someone",
     year: 1991
 }
+
+
+// post
 
 function postMovies(newMovie){
     const options = {
@@ -54,3 +59,41 @@ btnListEl.addEventListener("click", () => {
         listEl.insertAdjacentHTML("beforeend", markup)
     })
 })
+
+// patch
+
+function patchMovies(newMovie){
+    const options = {
+        method: "PATCH",
+        body: JSON.stringify(newMovie),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+}
+
+fetch(`${BASE_URL}/${patchMovies}`, options)
+.then(res => res.json())
+.then(patch => console.log(patch))
+.catch(error => console.log("ERROR" + error))
+
+formEl.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const newFilm = {
+       title: event.currentTarget.elements.title.value,
+       genre: event.currentTarget.elements.genre.value,
+       director: event.currentTarget.elements.director.value,
+       year: event.currentTarget.elements.year.value
+    }
+    deleteMovie(newFilm)
+    console.log(newFilm)
+})
+
+// delete
+
+function deleteMovie(id){
+    fetch(`${BASE_URL}/${id}`, {
+  method: "DELETE",
+}).then(() => console.log("Post deleted"))
+  .catch(error => console.log("Error:", error));
+}
